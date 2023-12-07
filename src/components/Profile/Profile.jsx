235e6a44d5102565f933888b8c9a72a5fb13892e
Holubchuk <sashagolubchuk3@gmail.com>
2023-12-07 15:10:25 -0500

    feature/Task-1/first-try

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export const Profile = ({ username, tag, location, avatar, stats  }) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src={avatar}
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export const user = {
+  "username": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+}
